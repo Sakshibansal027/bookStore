99463c60af63bd133a9c5f7eb2ba4607776f6d10
Sakshibansal027 <sakshibansal827@gmail.com>
2026-01-30 00:06:20 +0530

    fixed url

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import userRoute from "./route/user.route.js";
 const app = express();
 app.use(cors({
   origin: [
-    "https://bookstore-frontend-2pvl.onrender.com/"
+    "https://bookstore-frontend-2pvl.onrender.com"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
